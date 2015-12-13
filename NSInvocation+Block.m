ad19826d5062215b9c0dcd7ac8e92c99b0730e0c
Guo Yu <canopus4u@outlook.com>
2015-12-13 20:17:51 +0800

    Create NSInvocation+Block.m

---
+++ NSInvocation+Block.m
@@ -0,0 +1,56 @@
+//
+//  NSInvocation+Block.m
+//  NSInvocation+Block
+//
+//  Created by deput on 12/11/15.
+//  Copyright © 2015 deput. All rights reserved.
+//
+
+#import "NSInvocation+Block.h"
+#import <objc/runtime.h>
+
+struct Block_literal_1 {
+    void *isa; // initialized to &_NSConcreteStackBlock or &_NSConcreteGlobalBlock
+    int flags;
+    int reserved;
+    void (*invoke)(void *, ...);
+    struct Block_descriptor_1 {
+        unsigned long int reserved;     // NULL
+        unsigned long int size;         // sizeof(struct Block_literal_1)
+        // optional helper functions
+        // void (*copy_helper)(void *dst, void *src);     // IFF (1<<25)
+        // void (*dispose_helper)(void *src);             // IFF (1<<25)
+        // required ABI.2010.3.16
+        // const char *signature;                         // IFF (1<<30)
+        void* rest[1];
+    } *descriptor;
+    // imported variables
+};
+
+enum {
+    BLOCK_HAS_COPY_DISPOSE =  (1 << 25),
+    BLOCK_HAS_CTOR =          (1 << 26), // helpers have C++ code
+    BLOCK_IS_GLOBAL =         (1 << 28),
+    BLOCK_HAS_STRET =         (1 << 29), // IFF BLOCK_HAS_SIGNATURE
+    BLOCK_HAS_SIGNATURE =     (1 << 30),
+};
+
+static const char *__BlockSignature__(id blockObj)
+{
+    struct Block_literal_1 *block = (__bridge void *)blockObj;
+    struct Block_descriptor_1 *descriptor = block->descriptor;
+    assert(block->flags & BLOCK_HAS_SIGNATURE);
+    int offset = 0;
+    if(block->flags & BLOCK_HAS_COPY_DISPOSE)
+        offset += 2;
+    return (char*)(descriptor->rest[offset]);
+}
+
+@implementation NSInvocation (Block)
+
++ (instancetype) invocationWithBlock:(id) block
+{
+    NSInvocation* invocation = [NSInvocation invocationWithMethodSignature:[NSMethodSignature signatureWithObjCTypes:__BlockSignature__(block)]];
+    invocation.target = block;
+    return invocation;
+}
