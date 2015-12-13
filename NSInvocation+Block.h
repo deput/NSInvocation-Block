394da6e88fcbd22faab7cff8d369636c544381f2
Guo Yu <canopus4u@outlook.com>
2015-12-13 20:16:54 +0800

    Create NSInvocation+Block.h

---
+++ NSInvocation+Block.h
@@ -0,0 +1,14 @@
+//
+//  NSInvocation+Block.h
+//  NSInvocation+Block
+//
+//  Created by deput on 12/11/15.
+//  Copyright © 2015 deput. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSInvocation (Block)
++ (instancetype) invocationWithBlock:(id) block;
+//+ (instancetype) invocationWithBlockAndArguments:(id) block ,...; // impl later
+@end
